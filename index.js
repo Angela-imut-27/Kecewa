ee217e8ee93b316af80cdf118fac2453089587c4
AngelaImut <angelaimut665@gmail.com>
2026-03-03 20:43:15 +0000

    Initial commit Project MyTTS

---
+++ index.js
@@ -0,0 +1,7 @@
+import axios from 'axios';
+
+// Ini cuma contoh kode awal biar filenya nggak kosong
+console.log("Project MyTTS Berhasil Dijalankan!");
+console.log("Author: AngelaImut");
+
+// Nanti kodingan qwenLogic yang kita bahas tadi bisa kamu taruh di sini
